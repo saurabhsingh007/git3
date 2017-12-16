b29b055ff5ea1b8438322657b9a0351df35c3ba0
saurabh1.singh <saurabh1.singh@JILIT-DTP-0276.JIL>
2017-12-16 15:48:39 +0530

    create new class1 jhgjh

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -15,6 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        gfgdgdfgdfg
         // TODO code application logic here
     }
     
